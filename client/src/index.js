9757feb9f3f7bdcf0dab35e3f9e1cfe662bc74ee
Riddhick <riddhick14@gmail.com>
2023-07-03 00:34:51 +0530

    Tailwind Added

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<h1>Hello</h1>,document.getElementById("root"));
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+  root.render(
+    <App />
+  )

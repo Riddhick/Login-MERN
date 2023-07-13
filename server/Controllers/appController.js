7fbabe3da85c0699498aba5a4899a1e0a4f373f1
Riddhick <riddhick14@gmail.com>
2023-07-13 21:03:23 +0530

    Registration backend complete

--- server/Controllers/appController.js
+++ server/Controllers/appController.js
@@ -2,11 +2,20 @@ const Usermodel = require("../models/Usermodel");
 const bcrypt=require("bcryptjs");
 
 
-function register(req,res){
+async function register(req,res){
    try{
       const {Username, Password, Email , Profile}=req.body;
       
-      bcrypt.hash(Password,10,function(err,hash){
+      const checkUser= await Usermodel.findOne({Username});
+      if(checkUser){
+         return res.status(500).send({msg:'User already exists'})
+      }
+      const checkEmail=await Usermodel.findOne({Email});
+      if(checkEmail){
+         return res.status(500).send({msg:'Try another Email Id'})
+      }
+
+     bcrypt.hash(Password,10,function(err,hash){
          if (err){
             return res.status(500).send(err)
          }

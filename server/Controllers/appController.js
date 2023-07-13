d450c9dc48b5b6ac538c1867ac7226e8c25e661b
Riddhick <riddhick14@gmail.com>
2023-07-13 11:25:56 +0530

    Password Encryption Added

--- server/Controllers/appController.js
+++ server/Controllers/appController.js
@@ -5,16 +5,30 @@ const bcrypt=require("bcryptjs");
 function register(req,res){
    try{
       const {Username, Password, Email , Profile}=req.body;
-      //console.log(req.body)
-      const User= new Usermodel({
+      
+      bcrypt.hash(Password,10,function(err,hash){
+         if (err){
+            return res.status(500).send(err)
+         }
+         const User= new Usermodel({
+            Username,
+            Password:hash,
+            Email,
+            Profile:Profile ||""
+         });
+         User.save()
+            .then(result=>res.status(201).send({msg:"New User"}))
+            .catch(error=>res.status(500).send({error}))
+      })
+     /* const User= new Usermodel({
          Username,
-         Password,
+         Password:hashedPassword,
          Email,
          Profile:Profile ||""
       });
       User.save()
          .then(result=>res.status(201).send({msg:"New User"}))
-         .catch(error=>res.status(500).send({error}))
+         .catch(error=>res.status(500).send({error}))*/
    }
    catch(error){
       return res.status(500).send(error)
